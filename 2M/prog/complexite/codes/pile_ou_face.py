8f08bf26e2cbe01fb0c80927169464b622cacf3c
Vincent Keller <Vincent.Keller@epfl.ch>
2023-09-22 14:05:20 +0200

    modif pile ou face timing

--- 2M/prog/complexite/codes/pile_ou_face.py
+++ 2M/prog/complexite/codes/pile_ou_face.py
@@ -1,5 +1,7 @@
 import random
-borneSup = 1000000
+import time
+start = time.time()
+borneSup = 10000
 nbrPiles = 0
 nbrFaces = 0
 for i in range(borneSup):
@@ -8,6 +10,8 @@ for i in range(borneSup):
         nbrPiles = nbrPiles + 1
     else:
         nbrFaces = nbrFaces + 1
-
+end = time.time()
 print("Nombre de piles : ",nbrPiles)
 print("Nombre de faces : ",nbrFaces)
+print("Nombre de tirs  : ",borneSup)
+print("Time            : ",(end-start))
