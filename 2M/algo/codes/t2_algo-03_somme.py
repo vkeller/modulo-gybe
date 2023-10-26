90d38463f6cdbebe4cae036893a3e7122897d9c1
Vincent Keller <Vincent.Keller@epfl.ch>
2023-10-26 16:20:37 +0200

    added some code

--- 2M/algo/codes/t2_algo-03_somme.py
+++ 2M/algo/codes/t2_algo-03_somme.py
@@ -1,10 +1,10 @@
 longueur = 20
 nops = 0
-somme_des_carres = 0
+sdp = 0
 for i in range(longueur):
     for j in range(longueur):
         somme = i + j
-        somme_des_carres = somme_des_carres + somme
+        sdp = sdp + somme
         nops = nops + 2
-print(nops)
+print(sdp,nops)
         
