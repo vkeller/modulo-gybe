f8db13258978ff773368dd183e5cece84803f8e9
Vincent Keller <Vincent.Keller@epfl.ch>
2023-11-28 12:41:50 +0100

    Modif code monte-carlo 2 dés

--- 2M/strategies/codes/monte_carlo_des.py
+++ 2M/strategies/codes/monte_carlo_des.py
@@ -5,8 +5,13 @@ de1 = 0
 de2 = 0
 somme = 0
 nb_exp = int(input("Nombre d'expériences : "))
+reel = [(1/36)*100,(2/36)*100,(3/36)*100,(4/36)*100,(5/36)*100,(6/36)*100,(5/36)*100,(4/36)*100,(3/36)*100,(2/36)*100,(1/36)*100]
+
 repart = []
 values = []
+diff = []
+
+graph = False
 
 for i in range(13):
     repart.append(0)
@@ -20,10 +25,25 @@ for i in range(nb_exp):
 for i in range(13):
     repart[i] = repart[i]/nb_exp*100
 
-print(repart)
 
-plt.bar(values,repart)
-plt.xlabel('valeurs')
-plt.ylabel('nombres')
-plt.title('Distribution des sommes')
-plt.show()
+repart.pop(0)
+repart.pop(0)
+
+for i in range(11):
+    diff.append(repart[i]-reel[i])
+    reel[i] = format(reel[i],'.2f')
+    repart[i] = format(repart[i],'.2f')
+    diff[i] = format(diff[i],'.2f')
+    
+
+print(repart)
+print(reel)
+print(diff)
+
+
+if graph:
+    plt.bar(values,repart)
+    plt.xlabel('valeurs')
+    plt.ylabel('nombres')
+    plt.title('Distribution des sommes')
+    plt.show()
