f623b1ef49b8fd98c42db7fcf46ba6e94679744b
Vincent Keller <Vincent.Keller@epfl.ch>
2024-03-04 14:46:49 +0100

    Added Noll_90

--- 2M/esdn/DigitalArt/codes/noll_90.py
+++ 2M/esdn/DigitalArt/codes/noll_90.py
@@ -17,6 +17,10 @@ def rectangle(t,l1,l2):
         t.forward(l2)
         t.left(90)
 
+def triangle_rectangle(t,x1,y1,x2,y2):
+    t.goto(x1,y2)
+    t.goto(x2,y2)
+
 t = Turtle()
 t.speed(100)
 
@@ -33,18 +37,28 @@ rectangle(t,xmax-xmin,ymax-ymin)
 t.pensize(1)
 t.pencolor('blue')
 
+t.penup()
+xold = int(xmin/2)
+yold = int(ymin/2)
+t.goto(xold, yold)
+t.pendown()
+
+# Boucle du dessin
 for i in range(60):
     x = randint(1,(xmax-xmin))+xmin
     y = randint(1,(ymax-ymin))+ymin
     if x >= xmin-1 and x <= xmax+1 and y >= ymin-1 and y <= ymax+1 :
-        t.goto(x,y)
+#        t.goto(x,y)
+        triangle_rectangle(t,xold,yold,x,y)
+        xold = x
+        yold = y
     else:
         i -= 1
         
         
 screen = Screen()
 canvas = screen.getcanvas()
-fileName = "noll_2024"
+fileName = "noll_90_2024"
 canvas.postscript(file=fileName+'.eps', width=(xmax-xmin)*1.4, height=(ymax-ymin)*1.2)
 img = Image.open(fileName + '.eps') 
 img.save(fileName + '.jpg')  
