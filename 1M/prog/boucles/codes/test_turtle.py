838104ee31297e18b9349f2c706962f67332509d
Vincent Keller <Vincent.Keller@epfl.ch>
2025-08-05 14:51:09 +0200

    Modif conditions

---
+++ 1M/prog/boucles/codes/test_turtle.py
@@ -0,0 +1,23 @@
+from turtle import *
+t = Turtle()
+
+nbr = 5
+
+if nbr == 3 :
+    # Dessiner un triangle
+    t.forward(100)
+    t.left(120)
+    t.forward(100)
+    t.left(120)
+    t.forward(100)
+    t.left(120)
+else:
+    # Dessiner un carré
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
